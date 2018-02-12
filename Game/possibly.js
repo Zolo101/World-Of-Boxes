e1b2a6a2621aa3d3b5997a9e8ce1505dd14127ec
Zolo101 <umadeguy@gmail.com>
2018-02-12 14:37:35 +0000

    0.1.0beta.1

--- Game/possibly.js
+++ Game/possibly.js
@@ -1,7 +1,25 @@
-var boxes = 0;
+var box = {
+    "name": "Box",
+    "desc": "The thing your trying to get loads of.",
+    "ostorage": 50,
+    "rarity": "common",
+    "startvis": true,
+    "quantity": 0,
+    "bps": 0,
+}
+
+var steel = {
+    "name": "Steel",
+    "desc": "The compressed product of tons of boxes. Can be used to create machines.",
+    "ostorage": 10,
+    "rarity": "uncommon",
+    "startvis": false,
+    "quantity": 0,
+    "bps": 0,
+}
+
 var bpc = 1;
 var bps = 0;
-
 var productivehands = {
     "name": "Productive Hands",
     "desc": "Your manual box <br>making goes up by 1",
@@ -27,31 +45,33 @@ var machine580 = {
     "visible": false
 };
 
+function constructbox() {
 
-function createBox() {
-    boxes = boxes + bpc;
-    document.getElementById("game-counter").innerHTML = boxes;
-}
-
-function createbps() {
-    boxes = boxes + bps;
-    document.getElementById("game-counter").innerHTML = boxes;
-}
-
-function upgrade1() {
-    var upgrade1cost = Math.floor(25 * Math.pow(1.1, upgrade1s)); // the cost
-    if (boxes >= upgrade1cost) {
-        upgrade1s = upgrade1s + 1;
-        bpc = bpc + 1;
-        boxes = boxes - upgrade1cost;
-        document.getElementById("game-upgrade-button1").innerHTML = "Productive Hands (" + upgrade1s + ')<p id="game-main-button-desc"> + Your manual box <br>making goes up by 1</p><p id="game-upgrade-button1-cost">25 Boxes</p>';
-        document.getElementById("game-counter").innerHTML = boxes;
+    if (box.quantity < box.ostorage) {
+        box.quantity++;
     }
-    var upgrade1next = Math.floor(25 * Math.pow(1.1, upgrade1s));
-    document.getElementById("game-upgrade-button1-cost").innerHTML = "Cost: " + upgrade1next + " Boxes";
+
+    document.getElementById("game-box-counter").innerHTML = box.quantity;
+    document.getElementById("game-box-counter-limit").innerHTML = box.ostorage;
 }
+
+function compressbox(Number) {
+    if (box.quantity >= 10 * Number && steel.quantity <= 9) {
+        box.quantity = box.quantity - 10 * Number;
+        steel.quantity = steel.quantity + 1 * Number;
+        document.getElementById("game-box-counter").innerHTML = box.quantity;
+        document.getElementById("game-steel-counter").innerHTML = steel.quantity;
+    }
+}
+
+function constructbps() {
+    box.quantity = box.quantity + box.bps;
+    document.getElementById("game-box-counter").innerHTML = box.quantity;
+    document.getElementById("game-box-counter-limit").innerHTML = box.ostorage;
+}
+
+
 
 window.setInterval(function () {
-    createbps();
-    document.getElementById("game-bps").innerHTML = bps + "/s";
+    constructbps();
 }, 1000);
