86c66a69f082a902365f8c599b9be1a481fa1690
Zolo101 <umadeguy@gmail.com>
2018-06-05 17:28:50 +0100

    Rework mk 2

--- Game/possibly.js
+++ Game/possibly.js
@@ -3,25 +3,50 @@
 box.quantity = box.quantity + box.ps;
     document.getElementById("game-box-counter").innerHTML = box.quantity;
     document.getElementById("game-box-counter-limit").innerHTML = box.ostorage; */
+/*document.getElementById("button-makeabox").style.display = "none";*/
 
 var box = {
-    type:"Resource",
+    type: "Resource",
     totalbox: 0,
     quantity: 0,
     perclick: 1,
     persecond: 0
-}
+};
+
+var compressedbox = {
+    type: "Resource",
+    totalbox: 0,
+    quantity: 0,
+    persecond: 0
+};
 
 function makeabox() {
     box.quantity = box.quantity + box.perclick;
     box.totalbox = box.totalbox + box.perclick;
-    document.getElementById("boxes").innerHTML = box.quantity
+    document.getElementById("boxes").innerHTML = box.quantity;
+}
+
+function compressbox() {
+    if (box.quantity >= 9) {
+        box.quantity = box.quantity - 9;
+        compressedbox.quantity = compressedbox.quantity + 1;
+        document.getElementById("boxes").innerHTML = box.quantity;
+        document.getElementById("compressed-boxes").innerHTML = compressedbox.quantity
+        if (compressedbox.totalbox = 15 || compressedbox.quantity = 10) {
+            document.getElementById("compressed-boxes").style.display = "inline";
+        }
+    }
+}
+
+function makeascrewdriver() {
+    if (compressedbox.quantity >= 4) {
+        compressedbox.quantity = compressedbox.quantity - 4;
+
+    }
 }
 
 function tick() {
-    if (box.totalbox >= 10) {
-
-    }
+    document.getElementById("boxes").innerHTML = box.quantity;
 }
 
 
